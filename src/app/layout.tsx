f8782681a4c18ec386a957e180c68fb9bede2caa
mangrovelabs <mangrove@mangrovelabs.com.br>
2025-10-16 17:30:58 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import "../index.css";
 import type { ReactNode } from "react";
 
 export const metadata = {
-  title: "Criar Landing Page",
-  description: "Landing page migrada para Next.js",
+  title: "Mangrove Labs",
+  description: "Página institucional da Mangrove Labs, fábrica de software especializada em soluções digitais com alto desempenho e inovação.",
+    icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
